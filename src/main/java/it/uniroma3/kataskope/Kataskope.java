574607df92df9f9ab1a30c0fa08761c2a6eaa161
Andrea Olivieri <andrea.olivieri1991@gmail.com>
2016-07-07 14:34:49 +0200

    end

--- src/main/java/it/uniroma3/kataskope/Kataskope.java
+++ src/main/java/it/uniroma3/kataskope/Kataskope.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.tinkerpop.blueprints.impls.orient.OrientGraph;
 import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory;
 
 import it.uniroma3.tables.AnagraficaDealer;
@@ -11,7 +12,7 @@ import it.uniroma3.tables.AnagraficaDealer;
 public class Kataskope {
 
 	private static String MYSQL_URL = "jdbc:mysql://localhost:3306/Kataskopeo_hash?serverTimezone=UTC&autoReconnect=true&useSSL=false";
-	private static String ORIENTDB_URL = "remote:localhost/database/Kataskope";
+	private static String ORIENTDB_URL = "remote:localhost/database/Kataskopeo";
 
 	public static void main(String[] args) {
 		String mysql_username = args[0];
@@ -24,7 +25,7 @@ public class Kataskope {
 		try {
 			mysqlConnection = DriverManager.getConnection(MYSQL_URL, mysql_username, mysql_password);
 			System.out.println("Database connected!");
-			OrientGraphFactory orientDbFactory = new OrientGraphFactory(ORIENTDB_URL, orientdb_username, orientdb_password).setupPool(1,10);
+			OrientGraphFactory orientDbFactory = initOrientDB(orientdb_username, orientdb_password);
 			tablesFactory(mysqlConnection, orientDbFactory);
 			mysqlConnection.close();
 		} catch (SQLException e) {
@@ -34,6 +35,16 @@ public class Kataskope {
 
 	private static void tablesFactory(Connection mysqlConnection, OrientGraphFactory orientDbFactory) {
 		new AnagraficaDealer(mysqlConnection, orientDbFactory);
+	}
+	
+	private static OrientGraphFactory initOrientDB(String orientdb_username, String orientdb_password){
+		OrientGraphFactory orientDbFactory = new OrientGraphFactory(ORIENTDB_URL, orientdb_username, orientdb_password).setupPool(1,10);
+		try {
+			OrientGraph graph = orientDbFactory.getTx();
+		} catch (Exception e) {
+			System.out.println("Katascopeo non esistente");
+		}
+		return orientDbFactory;
 	}
 
 }
