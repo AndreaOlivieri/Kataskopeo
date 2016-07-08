9c6f80b8ba3629a95659040e63395a9ff6e1ae46
AME-DELVECCHIO <amerigo.delvecchio@gmail.com>
2016-07-08 18:44:40 +0200

    add classes (to defined methods)

--- src/main/java/it/uniroma3/tables/CRMC.java
+++ src/main/java/it/uniroma3/tables/CRMC.java
@@ -13,38 +13,55 @@ public class CRMC extends Table {
 	
 	public CRMC(Connection mysqlConnection, OrientGraphFactory orientDbFactory){
 		super(mysqlConnection, orientDbFactory);
-		//printResultSet(tableSet);
 	}
 	
 	@Override
-	protected void createVertexesAndEdges(ResultSetMetaData metaData, ResultSet resultSet) {
-		String column_name = "";
-		String column_value = "";
-		OrientGraph graph = orientDbFactory.getTx();
+	protected String sqlTable() {
+		return "SELECT COD_ID, COD_ID_PADRE, CODICE_NEW, CODFIS as CODICE_FISCALE, PIVA as PARTITA_IVA, RAGSOC as RAGIONE_SOCIALE, INDIRIZZO, LOCALITA, CAP, PROVINCIA, DSLOC, CANALE"
+		    + " FROM Kataskopeo_hash.ANAGRAFICA_DEALER";
+	}
+	
+	@Override
+	protected void createClasses() {
+		int columnCount;
+		String columnName = "";
+		try {
+			columnCount = metaData.getColumnCount();
+			graph.createVertexType("CODICE_ANAGRAFICA_DEALER");
+			for (int i = 4; i <= columnCount; i++) {
+				columnName = metaData.getColumnLabel(i);
+				graph.createVertexType(columnName);
+			}
+			graph.createEdgeType("HasInAnagraficaDealer");
+			graph.createEdgeType("HasOutAnagraficaDealer");
+			graph.commit();
+		} catch (SQLException e) {
+			graph.rollback();
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	protected void createVertexesAndEdges() {
+		String columnName = "";
+		String columnValue = "";
 		try {
 			int columnCount = metaData.getColumnCount();
 			String cod_id = resultSet.getString("COD_ID");
 			String cod_id_padre = resultSet.getString("COD_ID_PADRE");
 			String cod_new = resultSet.getString("CODICE_NEW");
-			OrientVertex primaryVertex = graph.addVertex("class:ANAGRAFICA_DEALER_ID", "cod_id", cod_id, "cod_id_padre", cod_id_padre, "codice_new", cod_new);
-			for (int i = 4; i <= columnCount; i++)
-			{
-			   column_name = metaData.getColumnLabel(i);
-			   column_value = resultSet.getString(column_name);
-			   OrientVertex secondVertex = graph.addVertex("class:"+column_name, "value", column_value);
+			OrientVertex primaryVertex = graph.addVertex("class:CODICE_ANAGRAFICA_DEALER", "cod_id", cod_id, "cod_id_padre", cod_id_padre, "codice_new", cod_new);
+			for (int i = 4; i <= columnCount; i++) {
+			   columnName = metaData.getColumnLabel(i);
+			   columnValue = resultSet.getString(columnName);
+			   OrientVertex secondVertex = addDistinctVertex(columnName, columnValue);
 			   graph.addEdge("class:HasInAnagraficaDealer", secondVertex, primaryVertex, "HasInAnagraficaDealer");
 			   graph.addEdge("class:HasOutAnagraficaDealer", primaryVertex, secondVertex, "HasOutAnagraficaDealer");
 			}
 			graph.commit();
 		} catch (SQLException e) {
+			graph.rollback();
 			e.printStackTrace();
 		}
 	}
-
-	@Override
-	protected String sqlTable() {
-		return "SELECT COD_ID, COD_ID_PADRE, CODICE_NEW, CODFIS, PIVA, RAGSOC, INDIRIZZO, LOCALITA, CAP, PROVINCIA, DSLOC, CANALE"
-				+ " FROM Kataskopeo_hash.ANAGRAFICA_DEALER";
-	}
-
 }
