5391fc22afbc214281dc44d40b4dad47907afa5b
Andrea Olivieri <andrea.olivieri1991@gmail.com>
2016-07-14 11:44:20 +0200

    almost done

--- src/main/java/it/uniroma3/user_relations/UserRelations.java
+++ src/main/java/it/uniroma3/user_relations/UserRelations.java
@@ -1,88 +1,92 @@
 package it.uniroma3.user_relations;
 
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 
-import com.orientechnologies.orient.core.db.record.OIdentifiable;
 import com.orientechnologies.orient.core.metadata.schema.OType;
-import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.sql.OCommandSQL;
-import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
 import com.tinkerpop.blueprints.Parameter;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.orient.OrientGraph;
 import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory;
+import com.tinkerpop.blueprints.impls.orient.OrientGraphNoTx;
 import com.tinkerpop.blueprints.impls.orient.OrientVertex;
 import com.tinkerpop.blueprints.impls.orient.OrientVertexType;
 
 public class UserRelations {
 
-	private static OrientGraph kataskopeo;
-	private static OrientGraph kataskopeoUserRelations;
+	private static OrientGraphFactory kataskopeoGraphFactory;
+	private static OrientGraphFactory kataskopeoUserRelationsGraphFactory;
+	private static String VERTEX_CLASS_NAME = "User";
+	private static String[] EDGE_CLASS_NAMES = {"ShortestPath"};
+	private static String SQL_SHORTEST_PATH = "SELECT expand(shortestPath( (SELECT * FROM ID_USER WHERE value=?), (SELECT * FROM ID_USER WHERE value=?), null, null, {'minDepth': 5, 'maxDepth': 5}))";
 
+	@SuppressWarnings("static-access")
 	public UserRelations(OrientGraphFactory kataskopeoGraphFactory, OrientGraphFactory kataskopeoUserRelationsGraphFactory) {
-		this.kataskopeo = kataskopeoGraphFactory.getTx();
-		this.kataskopeoUserRelations = kataskopeoUserRelationsGraphFactory.getTx();
-		initKataskopeoUserRelations();
-		path();
+		this.kataskopeoGraphFactory = kataskopeoGraphFactory;
+		this.kataskopeoUserRelationsGraphFactory = kataskopeoUserRelationsGraphFactory;
+		initSchema();
+		populateKataskopeoUserRelations();
+		createRelations();
 	} 
-
-	private void initKataskopeoUserRelations() {
-		String className = "USER";
-		OrientVertexType type = kataskopeoUserRelations.createVertexType(className);
+	
+	private void initSchema() {
+		OrientGraphNoTx kataskopeoUserRelations = kataskopeoUserRelationsGraphFactory.getNoTx();
+		for (String edgeClass : EDGE_CLASS_NAMES) {
+			kataskopeoUserRelations.createEdgeType(edgeClass);
+		}
+		OrientVertexType type = kataskopeoUserRelations.createVertexType(VERTEX_CLASS_NAME);
 		type.createProperty("user_id", OType.STRING);
-		kataskopeoUserRelations.createKeyIndex("user_id", Vertex.class, new Parameter<String, String>("class", className));
+		kataskopeoUserRelations.createKeyIndex("user_id", Vertex.class, new Parameter<String, String>("class", VERTEX_CLASS_NAME));
 		kataskopeoUserRelations.commit();
-		OCommandSQL query = new OCommandSQL("SELECT * FROM ID_USER");
+	}
+	
+	private void populateKataskopeoUserRelations() {
+		OrientGraph kataskopeo = kataskopeoGraphFactory.getTx();
+		OCommandSQL query = new OCommandSQL("SELECT * FROM ID_USER LIMIT 10");
 		Iterable<OrientVertex> user_ids = kataskopeo.command(query).execute();
+		
+		OrientGraph kataskopeoUserRelations = kataskopeoUserRelationsGraphFactory.getTx();
 		for (OrientVertex user : user_ids) {
 			kataskopeoUserRelations.addVertex("class:USER", "user_id", user.getProperty("value"));
 		}
 		kataskopeoUserRelations.commit();
 	}
 
-	private void path(){
+	private void createRelations(){
+		OrientGraph kataskopeo = kataskopeoGraphFactory.getTx();
+		OrientGraph kataskopeoUserRelations = kataskopeoUserRelationsGraphFactory.getTx();
+
 		OCommandSQL query = new OCommandSQL("SELECT * FROM USER");
 		Iterable<OrientVertex> user_idsFirst = kataskopeoUserRelations.command(query).execute();
-		Iterable<OrientVertex> user_idsSecond = user_idsFirst;
-		String sqlQuery = "SELECT expand(shortestPath( "
-				+"(SELECT * FROM ID_USER WHERE value=?),"
-				+"(SELECT * FROM ID_USER WHERE value=?),"
-				+"null, null, {'maxDepth': 10}))";
+		
 		String idFirst, idSecond = "";
 		for (OrientVertex userFirst : user_idsFirst) {
+			idFirst = userFirst.getProperty("user_id");
+			Iterable<OrientVertex> user_idsSecond = kataskopeoUserRelations.command(query).execute();
 			for (OrientVertex userSecond : user_idsSecond) {
-				idFirst = userFirst.getProperty("user_id");
 				idSecond = userSecond.getProperty("user_id");
 				if(!idFirst.equals(idSecond)) {
-					OCommandSQL queryCommand = new OCommandSQL(sqlQuery);
-					Iterable<OrientVertex> result = kataskopeo.command(queryCommand).execute(idFirst, idSecond);
-					kataskopeoUserRelations.addEdge("class:BONO", userFirst, userSecond, "BONA");
-					kataskopeoUserRelations.commit();
+					shortestPath(kataskopeo, kataskopeoUserRelations, userFirst, userSecond);
 				}
 			}
 		}
-//		String sqlQuery = "SELECT 
-//				+ "          FROM ( "
-//				+ "                  TRAVERSE out('Has_CODICE_TESTATA_DWI'), out('Has_NUM_DOCUMENTO_CLIENTE'), "
-//				+ "                           out('Has_CODICE_TESTATA_DWI'), out('Has_ID_CLIENTE') "
-//				+ "                      FROM ("
-//				+ "                              SELECT * "
-//				+ "                                FROM ID_USER "
-//				+ "                               WHERE VALUE = ?"
-//				+ "                           ) "
-//				+ "               ) "
-//				+ "          WHERE @class = 'ID_USER' AND value <> ?";
-//		OCommandSQL queryCommand = new OCommandSQL(sqlQuery);
-//		Iterable<OrientVertex> ciao = kataskopeo.command(queryCommand).execute("7-6HL1EFD", "7-6HL1EFD");
-//		int count = 0;
-//		for (OrientVertex dio : ciao) {
-//			System.out.println(dio.getProperty("$path"));
-//		}
-//		System.out.println("goal");
+		kataskopeoUserRelations.commit();
 	}
 
-
+	private void shortestPath(OrientGraph kataskopeo, OrientGraph kataskopeoUserRelations, OrientVertex userFirst, OrientVertex userSecond) {
+		String idFirst = userFirst.getProperty("user_id");
+		String idSecond = userSecond.getProperty("user_id");
+		OCommandSQL queryCommand = new OCommandSQL(SQL_SHORTEST_PATH);
+		Iterable<OrientVertex> result = kataskopeo.command(queryCommand).execute(idFirst, idSecond);
+		if(result.iterator().hasNext()) {
+			for (OrientVertex dio : result) {
+				System.out.print(dio.getProperty("value")+" ");
+				kataskopeoUserRelations.addEdge("class:Relation", userFirst, userSecond, "Relation");
+			}
+			System.out.println("");
+		} else {
+			System.out.println("DIO BONO");
+		}
+	}
+	
 }
